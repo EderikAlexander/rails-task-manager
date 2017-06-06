336f23a5d2f352ff1f745ca21ae8088acb8390ea
Ederik van Koldenhoven <e.vankoldenhoven@gmail.com>
2017-06-06 14:04:31 +0200

    solved rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  # get 'tasks', to: 'tasks#index'
 
-  get 'tasks/show'
+  # post 'tasks', to: 'tasks#create'
 
-  get 'tasks/new'
+  #  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
-  get 'tasks/create'
+  # get 'tasks/new', to: 'tasks#new'
 
-  get 'tasks/edit'
+  # get 'tasks/:id/edit', to: 'tasks#edit'
 
-  get 'tasks/update'
 
-  get 'tasks/destroy'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+   resources :tasks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
